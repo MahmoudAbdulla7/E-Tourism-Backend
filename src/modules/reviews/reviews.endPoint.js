cef85b1e75a5e44b33a10ba56ef2c28a44c3fc48
MahmoudAbdalla7 <mahmoudapdallah629@gmail.com>
2024-06-21 07:03:22 +0300

    test2

--- src/modules/reviews/reviews.endPoint.js
+++ src/modules/reviews/reviews.endPoint.js
@@ -3,5 +3,5 @@ import {roles} from '../../middleware/auth.js'
 export const endPoint ={
     create:[roles.User],
     update:[roles.User],
-    delete:[roles.User],
+    delete:[roles.User,roles.Admin],
 }
